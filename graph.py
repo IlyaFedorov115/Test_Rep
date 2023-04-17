c03eae70be542648b5d14652ab991a0757840eb8
IlyaFedorov115 <42892348+IlyaFedorov115@users.noreply.github.com>
2023-04-17 16:18:14 +0300

    Update graph.py

--- graph.py
+++ graph.py
@@ -267,15 +267,12 @@ def extract_bps(string):
 def get_pps(string):
     # Ищем первое вхождение "PPS:" в строке
     start_index = string.find("PPS:") + 5
-    
     # Ищем первый символ, отличный от цифр и точки, после значения PPS
     end_index = start_index
     while end_index < len(string) and string[end_index].isdigit() or string[end_index] == ".":
         end_index += 1
-    
     # Извлекаем значение PPS из строки
     pps_value = string[start_index:end_index]
-    
     # Конвертируем PPS в число
     if pps_value[-1] == 'k':
         pps_value = float(pps_value[:-1]) * 1000
@@ -283,5 +280,4 @@ def get_pps(string):
         pps_value = float(pps_value[:-1]) * 1000000
     else:
         pps_value = float(pps_value)
-    
     return pps_value
